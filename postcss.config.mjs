c79383fa19ac79e23ce91eb22e737d79995986c2
Ambaaq-Ajibike <ajibikeambaaq@gmail.com>
2025-12-17 13:07:08 +0100

    Init

---
+++ postcss.config.mjs
@@ -0,0 +1,10 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+const config = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
+
+export default config;
